18d0aa1c4ad11f76c1204988326489a02e21ccea
Furkan Emmezoğlu <furkan.emmezoglu@titra.com.tr>
2022-01-24 23:19:06 +0300

    some react code

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [coins, setCoins] = useState();
+
+  const fetchData = async () => {
+    const url = "http://localhost:4445/";
+    const query = `
+    query  {
+      AllCoins {
+        asset_id
+        name
+        price
+        gain
+        time
+      }
+    }
+    `;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+    });
+    const responseBody = await response.json();
+    setCoins(responseBody?.data?.AllCoins);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {coins && 
+        <table> 
+          <tbody>{coins.map((coin, index) => (
+            <tr key = {index}>
+              <td>
+                {coin.asset_id}
+              </td>
+              <td>
+                {coin.name}
+              </td>
+              <td>
+                {coin.price}
+              </td>
+              <td>
+                {coin.gain}
+              </td>
+              <td>
+                {coin.time}
+              </td>
+            </tr>
+          ))}</tbody>
+        </table>
+      }
+    </>
   );
 }
 
