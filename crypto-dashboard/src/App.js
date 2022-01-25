52246ef27aa5aeaaf36fb9a9bcd2ecab690e22e0
Furkan Emmezoğlu <furkan.emmezoglu@titra.com.tr>
2022-01-25 23:34:32 +0300

    apollo gateway working and some react properties

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,64 +1,25 @@
 import logo from "./logo.svg";
+import Header from "./components/header/header";
+import { BrowserRouter,Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { useEffect, useState } from "react";
+import Dashboard from "./components/dashboard/dashboard";
+import Landingpage from "./components/landing-page/landing-page";
 
 function App() {
-  const [coins, setCoins] = useState();
-
-  const fetchData = async () => {
-    const url = "http://localhost:4445/";
-    const query = `
-    query  {
-      AllCoins {
-        asset_id
-        name
-        price
-        gain
-        time
-      }
-    }
-    `;
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    });
-    const responseBody = await response.json();
-    setCoins(responseBody?.data?.AllCoins);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  
 
   return (
     <>
-      {coins && 
-        <table> 
-          <tbody>{coins.map((coin, index) => (
-            <tr key = {index}>
-              <td>
-                {coin.asset_id}
-              </td>
-              <td>
-                {coin.name}
-              </td>
-              <td>
-                {coin.price}
-              </td>
-              <td>
-                {coin.gain}
-              </td>
-              <td>
-                {coin.time}
-              </td>
-            </tr>
-          ))}</tbody>
-        </table>
-      }
+    <Header></Header>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/home" element={<Landingpage />}>
+       
+      </Route>
+    </Routes>
+  </BrowserRouter>
     </>
   );
 }
